832e42bfa1f943ee700a65f45428ed633b6a06c1
akhil-naidu <kaparapu.akhilnaidu@gmail.com>
2023-08-10 12:52:32 +0530

    added: alert-dialog

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,10 +1,39 @@
 import { Button } from 'ui';
 
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from 'ui/components/ui/alert-dialog';
+
 export default function Page() {
   return (
     <>
       <h1>Docs</h1>
-      <Button>Click me</Button>
+
+      <AlertDialog>
+        <AlertDialogTrigger>Show Dialog</AlertDialogTrigger>
+
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction>Continue</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 }
